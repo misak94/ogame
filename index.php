157b9af2846155ebb72030ea47e86aef3050af7e
veim01 <misakadioseznam.cz>
2022-07-12 22:46:22 +0200

    test

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+
+// definice tridy connectDB
+
+class connectDB {
+
+    //konstanty
+    public $promena1;
+
+    public function __construct(){
+
+    }
+
+    public function __destruct()
+    {
+
+    }
+
+    public function vypis($text){
+        echo $text;
+    }
+}
+
+?>
